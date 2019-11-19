906ac4fbfbed11af15b992b5cd6bac3eefa53929
Mason Wheeler <mwheeler050@gmail.com>
2019-11-19 12:54:48 -0600

    added comments.

--- Rectangle/src/edu/missouristate/chapter6/Circle.java
+++ Rectangle/src/edu/missouristate/chapter6/Circle.java
@@ -6,36 +6,63 @@ public class Circle {
 	private double radius;
 	private final double PI = 3.14159;
 	
+	/**
+		no args constructor
+	 */
 	public Circle() 
 	{
 		radius = 0.0;
 	}
 	
+	/**
+	 * Constructor that takes rad as argument to set the radius of the Circle object.
+	 * @param rad double
+	 */
 	public Circle(double rad)
 	{
 		radius = rad;
 	}
 	
+	/**
+	 * Sets radius of Circle object.
+	 * @param rad double
+	 */
 	public void setRadius(double rad) 
 	{
 		radius = rad;
 	}
 	
+	/**
+	 * 
+	 * @return double Radius of the circle.
+	 */
 	public double getRadius() 
 	{
 		return radius;
 	}
 	
+	/**
+	 * 
+	 * @return double Area of the circle.
+	 */
 	public double getArea() 
 	{
 		return PI * radius * radius;
 	}
 	
+	/**
+	 * 
+	 * @return double Diameter of the circle.
+	 */
 	public double getDiameter()
 	{
 		return radius * 2;
 	}
 	
+	/**
+	 * 
+	 * @return double Circumference of the circle.
+	 */
 	public double getCircumference()
 	{
 		return 2 * PI * radius;
